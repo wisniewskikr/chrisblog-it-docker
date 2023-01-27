f480d69039ad68210a437004f502627e84794a7e
Wisniewski <wisniewk@emea.roche.com>
2023-01-27 14:16:52 +0100

    Update project springboot-helloworld-docker-volumes-named

--- spring-boot/springboot-helloworld-docker-volumes-named/src/main/java/com/example/controllers/HelloWorldController.java
+++ spring-boot/springboot-helloworld-docker-volumes-named/src/main/java/com/example/controllers/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.example.controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -25,8 +28,37 @@ public class HelloWorldController {
 	@RequestMapping(value="/")
 	public String helloWorld() throws IOException {		
 		
-		fileService.writeToFile(filePath, "Hello World!");
-		return fileService.readFromFile(filePath.toString());
+		String contentMessage = getContentMessage();
+		fileService.writeToFile(filePath, contentMessage);
+		return contentMessage;
+		
+	}
+	
+	private String getContentMessage() {
+		
+		List<String> list = getContentList();
+		
+		int index = list.size() + 1;
+		list.add("Hello World " + index);
+		String contentMessage = String.join(", ", list);
+		
+		return contentMessage;
+		
+	}
+	
+	private List<String> getContentList() {
+		
+		List<String> list = new ArrayList<String>();
+		
+		String content = fileService.readFromFile(filePath.toString());
+		if (content != null && !"".equals(content)) {
+			
+			content = fileService.readFromFile(filePath.toString());
+			list = new ArrayList<String>(Arrays.asList(content.split(",")));
+			
+		}
+		
+		return list;
 		
 	}
 	

63df18da3ec935a002788fa8f9cd7b6bcdf59a8e
Wisniewski <wisniewk@emea.roche.com>
2023-01-27 16:16:05 +0100

    Update project springboot-helloworld-docker-volumes-named

--- spring-boot/springboot-helloworld-docker-volumes-named/src/main/java/com/example/controllers/HelloWorldController.java
+++ spring-boot/springboot-helloworld-docker-volumes-named/src/main/java/com/example/controllers/HelloWorldController.java
@@ -5,18 +5,24 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.services.FileService;
+import com.example.services.jsons.HelloWorldJson;
 
 @RestController
 public class HelloWorldController {
 	
-	@Value("${file.path}")
-	private String filePath;
+	@Value("${file.path.not.volume}")
+	private String filePathNotVolume;
+	
+	@Value("${file.path.volume}")
+	private String filePathVolume;
 	
 	private FileService fileService;
 	
@@ -24,19 +30,33 @@ public class HelloWorldController {
 	public HelloWorldController(FileService fileService) {
 		this.fileService = fileService;
 	}
-
+	
 	@RequestMapping(value="/")
-	public String helloWorld() throws IOException {		
+	public void init(HttpServletResponse response) throws IOException {	
+		response.sendRedirect("/not-stored-as-volume");
+	}
+
+	@RequestMapping(value="/not-stored-as-volume")
+	public HelloWorldJson notStoredAsVolume() throws IOException {		
 		
-		String contentMessage = getContentMessage();
-		fileService.writeToFile(filePath, contentMessage);
-		return contentMessage;
+		String contentMessage = getContentMessage(filePathNotVolume);
+		fileService.writeToFile(filePathNotVolume, contentMessage);
+		return new HelloWorldJson("File not stored as Volume. Press F5 to add new Greeting.", contentMessage);
 		
 	}
 	
-	private String getContentMessage() {
+	@RequestMapping(value="/stored-as-volume")
+	public HelloWorldJson storedAsVolume() throws IOException {		
 		
-		List<String> list = getContentList();
+		String contentMessage = getContentMessage(filePathVolume);
+		fileService.writeToFile(filePathVolume, contentMessage);
+		return new HelloWorldJson("File stored as Volume. Press F5 to add new Greeting.", contentMessage);
+		
+	}
+	
+	private String getContentMessage(String path) {
+		
+		List<String> list = getContentList(path);
 		
 		int index = list.size() + 1;
 		list.add("Hello World " + index);
@@ -46,14 +66,14 @@ public class HelloWorldController {
 		
 	}
 	
-	private List<String> getContentList() {
+	private List<String> getContentList(String path) {
 		
 		List<String> list = new ArrayList<String>();
 		
-		String content = fileService.readFromFile(filePath.toString());
+		String content = fileService.readFromFile(path);
 		if (content != null && !"".equals(content)) {
 			
-			content = fileService.readFromFile(filePath.toString());
+			content = fileService.readFromFile(path);
 			list = new ArrayList<String>(Arrays.asList(content.split(",")));
 			
 		}
